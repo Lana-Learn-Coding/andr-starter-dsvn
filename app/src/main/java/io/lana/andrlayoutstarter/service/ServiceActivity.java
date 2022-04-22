6c8eac5aa5ea9cad76e566e9a3357c3d0bdd6faa
toannd <toannd@eway.vn>
2022-04-22 08:18:27 +0700

    feat: use text in strings.xml instead of class

--- app/src/main/java/io/lana/andrlayoutstarter/service/ServiceActivity.java
+++ app/src/main/java/io/lana/andrlayoutstarter/service/ServiceActivity.java
@@ -7,11 +7,9 @@ import io.lana.andrlayoutstarter.NavigableActivity;
 import io.lana.andrlayoutstarter.R;
 
 public class ServiceActivity extends NavigableActivity {
-    private static final New loremNew = new New(
-            "Tàu chất lượng cao vận tải Hà Nội",
-            "Một thực tế đã có từ lâu rằng người đọc sẽ bị phân tâm bởi nội dung có thể đọc được của một trang khi nhìn vào bố cục của nó. Điểm đáng chú ý của việc sử dụng Lorem Ipsum là nó có sự phân bố các chữ cái bình thường ít nhiều, trái ngược với việc sử dụng 'Nội dung ở đây, nội dung ở đây', khiến nó trông giống như tiếng Anh có thể đọc được."
-    );
-    private static final New[] news = new New[]{loremNew, loremNew};
+    private final New loremNew = new New(getString(R.string.news_lorem_title), getString(R.string.news_lorem_content));
+
+    private final New[] news = new New[]{loremNew, loremNew};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
