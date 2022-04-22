50ebcd4c558acaa0fb33f2334e020a634e0c8421
toannd <toannd@eway.vn>
2022-04-22 09:24:24 +0700

    feat: add simple popup menu

--- app/src/main/java/io/lana/andrlayoutstarter/service/NewActivity.java
+++ app/src/main/java/io/lana/andrlayoutstarter/service/NewActivity.java
@@ -1,5 +1,7 @@
 package io.lana.andrlayoutstarter.service;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -9,18 +11,17 @@ import java.util.List;
 import io.lana.andrlayoutstarter.NavigableActivity;
 import io.lana.andrlayoutstarter.R;
 
-public class ServiceActivity extends NavigableActivity {
+public class NewActivity extends NavigableActivity {
     private final List<New> news = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_service);
+        setContentView(R.layout.activity_new);
 
-
-        news.add(new New(getString(R.string.news_lorem_title), getString(R.string.news_lorem_content)));
-        news.add(new New(getString(R.string.news_lorem_title), getString(R.string.news_lorem_content)));
-        news.add(new New(getString(R.string.news_lorem_title), getString(R.string.news_lorem_content)));
+        for (int i = 0; i < 10; i++) {
+            news.add(new New(getString(R.string.news_lorem_title), getString(R.string.news_lorem_content)));
+        }
         ListView listNews = findViewById(R.id.list_news);
         listNews.setAdapter(new NewAdapter(this, news));
     }
--- app/src/main/java/io/lana/andrlayoutstarter/service/NewAdapter.java
+++ app/src/main/java/io/lana/andrlayoutstarter/service/NewAdapter.java
@@ -1,11 +1,14 @@
 package io.lana.andrlayoutstarter.service;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +16,7 @@ import androidx.annotation.Nullable;
 import java.util.Collection;
 import java.util.List;
 
+import io.lana.andrlayoutstarter.FormActivity;
 import io.lana.andrlayoutstarter.R;
 
 public class NewAdapter extends ArrayAdapter<New> {
@@ -37,6 +41,25 @@ public class NewAdapter extends ArrayAdapter<New> {
         TextView textTitle = convertView.findViewById(R.id.title);
         if (textTitle != null) textTitle.setText(item.getTitle());
         if (textContent != null) textContent.setText(item.getContent());
+
+        convertView.setOnLongClickListener(view -> {
+            Context context = getContext();
+
+            PopupMenu popupMenu = new PopupMenu(context, view);
+            popupMenu.getMenuInflater().inflate(R.menu.news_menu, popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(menuItem -> {
+                int id = menuItem.getItemId();
+                if (id == R.id.item_view_all) {
+                    context.startActivity(new Intent(context, NewActivity.class));
+                    return true;
+                }
+
+                Toast.makeText(context, menuItem.getTitle() + ": " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            });
+            popupMenu.show();
+            return true;
+        });
 
         return convertView;
     }
