a23640210af7f3fc04d92d90ffdb9f531f666cfd
toannd <toannd@eway.vn>
2022-04-26 09:30:54 +0700

    feat: only fake history if no history present

--- app/src/main/java/io/lana/andrlayoutstarter/booking/BookingHistoryActivity.java
+++ app/src/main/java/io/lana/andrlayoutstarter/booking/BookingHistoryActivity.java
@@ -23,19 +23,21 @@ public class BookingHistoryActivity extends NavigableActivity {
         MainApplication application = (MainApplication) getApplicationContext();
 
         adapter = new BookingHistoryAdapter(this, application.getTickets());
-        for (int i = 0; i < 3; i++) {
-            BookingTicket ticket = new BookingTicket();
-            ticket.setId("DSVN" + Faker.instance().number().digits(8));
-            ticket.setName(Faker.instance().name().fullName());
-            ticket.setPhone(Faker.instance().phoneNumber().phoneNumber());
-            ticket.setFrom(Faker.instance().options().nextElement(application.getCities()));
-            ticket.setTo(Faker.instance().options().nextElement(application.getCities()));
-            ticket.setOneWay(Faker.instance().bool().bool());
-            ticket.setStartDate(Faker.instance().date().future(5, TimeUnit.DAYS).toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
-            ticket.setEndDate(Faker.instance().date().future(20, 8, TimeUnit.DAYS).toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
-            ticket.setChildrenCount(Faker.instance().number().numberBetween(1, 5));
-            ticket.setPeopleCount(Faker.instance().number().numberBetween(1, 5));
-            adapter.add(ticket);
+        if (application.getTickets().size() == 0) {
+            for (int i = 0; i < 3; i++) {
+                BookingTicket ticket = new BookingTicket();
+                ticket.setId("DSVN" + Faker.instance().number().digits(8));
+                ticket.setName(Faker.instance().name().fullName());
+                ticket.setPhone(Faker.instance().phoneNumber().phoneNumber());
+                ticket.setFrom(Faker.instance().options().nextElement(application.getCities()));
+                ticket.setTo(Faker.instance().options().nextElement(application.getCities()));
+                ticket.setOneWay(Faker.instance().bool().bool());
+                ticket.setStartDate(Faker.instance().date().future(5, TimeUnit.DAYS).toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+                ticket.setEndDate(Faker.instance().date().future(20, 8, TimeUnit.DAYS).toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+                ticket.setChildrenCount(Faker.instance().number().numberBetween(1, 5));
+                ticket.setPeopleCount(Faker.instance().number().numberBetween(1, 5));
+                adapter.add(ticket);
+            }
         }
         ListView listHistory = findViewById(R.id.list_history);
         listHistory.setAdapter(adapter);
